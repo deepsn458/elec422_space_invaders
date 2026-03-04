394054171e6b746602f1372b4e24a1478f6a1807
4439-A1 <43lancelot@gmail.com>
2026-03-03 19:16:57 -0600

    Uploaded Player Bullet files

---
+++ player_bullet/Synthesis/player_bullet_fsm.v
@@ -0,0 +1,96 @@
+///////////////////////////////////////////
+// player_bullet.v
+//
+// Written: ely2@rice.edu
+// Created: 2 Mar 2026
+//
+// Purpose: FSM for player bullet.
+// 
+//////////////////////////////////////////////////////////////////////////////////////////////// test
+
+module player_bullet_fsm(
+    input  wire             clka, clkb,                         // Input clocks
+    input  wire             reset, fire,                        // Global control and reset signals
+    input  wire [5:0]       player_coord_x,                     // Player bullet X coordinate
+    input  wire [5:0]       player_coord_y,                     // Player bullet Y coordinate
+    input wire              player_bullet_collision,            // Signal to indicate if the player bullet has collided with an invader
+    input wire              shield_bullet_collision,            // Signal to indicate if the player bullet has collided with a shield
+    output reg              display,                            // Signal to indicate if the player bullet should be displayed on the screen
+    output reg  [5:0]       player_bullet_coord_x,              // Player bullet X coordinate
+    output reg  [5:0]       player_bullet_coord_y,              // Player bullet Y coordinate
+    output reg  [1:0]       state                               // Current state of this player bullet
+    );
+
+    reg [1:0] temp_state;           // To be combinationally calculated
+    reg [1:0] next_state;           // To be sequentially assigned
+
+    // parameter SIZE = 2;
+    parameter INIT  = 2'b00, FIRING = 2'b01;
+    parameter Y_OFFSET = 3, Y_MAX = 31;
+
+    // Detection for checking if collision happened in invader bounding box
+    wire collide, boundary;
+    assign collide = player_bullet_collision | shield_bullet_collision;
+    assign boundary = (player_bullet_coord_y >= Y_MAX);     // Check if bullet has reached the top of the screen
+
+    // Combinational logic to calculate upcoming tempt state
+    always @(*) begin
+        case(state)
+
+        INIT: begin
+            if (fire) begin
+            temp_state = FIRING;
+            end else begin
+            temp_state = INIT;
+            end
+        end
+
+        FIRING: begin
+            if (collide | boundary) begin
+            temp_state = INIT;
+            end else begin
+            temp_state = FIRING;
+            end
+        end
+
+        default: temp_state = INIT;
+        endcase
+    end
+
+    // Sequential logic to set next_state <= temp_state or respond to restart signal
+    always @ (negedge clka) begin : FSM_SEQA
+        if (reset) begin
+            next_state <= INIT;
+        end else begin
+            next_state <= temp_state;
+        end
+    end
+
+    // Sequential logic to set outputs
+    always @ (negedge clkb) begin : FSM_SEQB            // clkb active signals are: Load_Temp, Accumulate
+        if (reset) begin
+            state <= INIT;
+            player_bullet_coord_x <= 0;
+            player_bullet_coord_y <= 0;
+            display <= 0;
+        end else begin
+
+        case(next_state)
+            INIT: begin
+                state <= next_state;
+                player_bullet_coord_x <= player_coord_x;
+                player_bullet_coord_y <= player_coord_y;
+                display <= 0;
+            end
+            FIRING: begin
+                state <= next_state;
+                player_bullet_coord_x <= player_bullet_coord_x;
+                player_bullet_coord_y <= player_bullet_coord_y + Y_OFFSET;
+                display <= 1;
+            end
+        endcase
+        end
+    end
+
+
+endmodule
