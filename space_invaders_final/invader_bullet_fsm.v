f0e4abe823053f19ad71d002cd4fefb5e73d0576
4439-A1 <43lancelot@gmail.com>
2026-04-14 18:04:30 -0500

    updated invader bullet fsm

--- space_invaders_final/invader_bullet_fsm.v
+++ space_invaders_final/invader_bullet_fsm.v
@@ -11,7 +11,7 @@
 module invader_bullet_fsm
 #(
     parameter BULLET_Y_OFFSET = 1,
-    parameter BOUNDARY_Y = 1,
+    parameter BOUNDARY_Y = 0,
     parameter START_X = 5,
     parameter START_Y = 25
 )(
@@ -33,8 +33,6 @@ module invader_bullet_fsm
     reg [1:0] temp_state;
     reg [1:0] next_state;
 
-    reg goback;
-
     //State parameters
     parameter INITIAL = 2'b00, FIRING = 2'b01;
 
@@ -48,7 +46,7 @@ module invader_bullet_fsm
             end
         end
         FIRING: begin
-            if (goback) begin
+            if ((invaderbullet_player_collision_signal | invaderbullet_shield_collision_signal)|| invader_bullet_coord_y == BOUNDARY_Y) begin
                 temp_state = INITIAL;
             end else begin
                 temp_state = FIRING;
@@ -75,18 +73,14 @@ module invader_bullet_fsm
                 invader_bullet_display <= 1'b0;
                 invader_bullet_coord_x <= closest_invader_coord_x;
                 invader_bullet_coord_y <= closest_invader_coord_y;
-                goback <= 1'b0;
             end
             FIRING: begin
                 state <= next_state;
                 invader_bullet_display <= 1'b1;
-                if (invaderbullet_player_collision_signal || invaderbullet_shield_collision_signal || invader_bullet_coord_y == BOUNDARY_Y) begin
-                    invader_bullet_coord_x <= closest_invader_coord_x;
-                    invader_bullet_coord_y <= closest_invader_coord_y;
-                    goback <= 1'b1;
-                end else begin
+                if (invader_bullet_coord_y >= BOUNDARY_Y) begin
                     invader_bullet_coord_y <= invader_bullet_coord_y - BULLET_Y_OFFSET;
                 end
+                
             end
             default: state <= INITIAL;
         endcase
